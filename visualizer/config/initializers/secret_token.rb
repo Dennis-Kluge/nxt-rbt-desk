3f67215ea08d408988076b3626126e212b48b0f6
Daniel Becker <daniel.becker@me.com>
2012-07-09 19:38:28 +0200

    Created Empty visualizer project

---
+++ visualizer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visualizer::Application.config.secret_token = '2fb3c8561240fc951d7a71c9bb75fb2990e407c3b481c5c788cb949e7aa8c9d2c6564fb1beafee3afd6398c1085d23d5d48459092af5c4a4c2ac7784fad2472b'
